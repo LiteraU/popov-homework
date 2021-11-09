cce7e738a8672fc7aa5dc55f4fab30fd66af4449
Valery Popov <valery@ariv.al>
2021-11-09 15:14:53 +0300

    feat(Promise): Implement promise work

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function slip (ms: number) {
+    return new Promise<void>((resolve) => {
+        setTimeout(() => {
+            resolve()
+        },ms)
+    })
+}
+
+  Promise.all([slip(2000), slip(5000)])
+    .then(() => {
+      console.log('all Promises')
+  })
+
+  Promise.race([slip(2000), slip(5000)])
+    .then(() => {
+        console.log('race Promises')
+    })
+
   return (
     <div className="App">
       <header className="App-header">
