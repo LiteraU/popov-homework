a54d62bb40705224888546912937b76608e148a2
Valery Popov <valery@ariv.al>
2021-11-09 16:27:31 +0300

    feat(Proxy): Implement proxy work

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,48 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function sum(a: number, b: number) {
+    return a + b;
+  }
+
+  function minus(a: number, b: number) {
+    return a - b;
+  }
+
+  function multiply(a: number, b: number) {
+    return a * b;
+  }
+
+  let sumHandler = {
+    apply: function(target: (arg0: number, arg1: number) => number, thisArg: any, argumentsList: number[]) {
+      console.log(`Calculate sum: ${argumentsList}`);
+      return argumentsList[0] + argumentsList[1];
+    }
+  };
+
+  let minusHandler = {
+    apply: function(target: (arg0: number, arg1: number) => number, thisArg: any, argumentsList: number[]) {
+      console.log(`Calculate minus: ${argumentsList}`);
+      return argumentsList[0] - argumentsList[1];
+    }
+  };
+
+  let multiplyHandler = {
+    apply: function(target: (arg0: number, arg1: number) => number, thisArg: any, argumentsList: number[]) {
+      console.log(`Calculate multiply: ${argumentsList}`);
+      return argumentsList[0] * argumentsList[1];
+    }
+  };
+
+  let proxySum = new Proxy(sum, sumHandler);
+  let prosyMinus = new Proxy(minus, minusHandler)
+  let proxyMultiply = new Proxy(multiply, multiplyHandler)
+
+  console.log(proxySum(1, 2));
+  console.log(prosyMinus(4, 2));
+  console.log(proxyMultiply(4, 2));
+
   return (
     <div className="App">
       <header className="App-header">
